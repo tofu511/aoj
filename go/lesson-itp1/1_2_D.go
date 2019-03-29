3cef104913e442f2439f7de5b1c74c2df1e2c6f3
tofu511 <mokmok511@gmail.com>
2019-03-30 01:01:59 +0900

    solved ITP1_2_D

--- go/lesson-itp1/1_2_D.go
+++ go/lesson-itp1/1_2_D.go
@@ -4,6 +4,15 @@ import "fmt"
 
 // https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/2/ITP1_2_D
 func main() {
-	var w, h, x, y, z int
-	fmt.Scan(&w, &h, &x, &y, &z)
+	var w, h, x, y, r int
+	fmt.Scan(&w, &h, &x, &y, &r)
+
+	xAxis := x - r >= 0 && x + r <= w
+	yAxis := y - r >= 0 && y + r <= h
+
+	if xAxis && yAxis {
+		fmt.Printf("Yes\n")
+	} else {
+		fmt.Printf("No\n")
+	}
 }
